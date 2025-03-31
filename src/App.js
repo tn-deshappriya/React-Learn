0bcf79f2bc65fb8fb0efedb24d870438e37440eb
Mr.No-Hat <thiniranilushandeshappriya@gmail.com>
2025-03-31 13:12:31 +0530

    8.prettierrc file created and show employee list with role.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
-import './App.css';
-import Employee from './Employee';
+import "./App.css";
+import Employee from "./Employee";
 
 function App() {
-  const employee =true;
+  const nameList = [
+    {
+      name: "john",
+      role: "intern",
+    },
+    {
+      name: "Thinira",
+      role: "Developer",
+    },
+    {
+      name: "Dinura",
+      role: "SeDev",
+    },
+    {
+      name: "Himasha",
+    },
+    {
+      name: "Sunil",
+      role: "",
+    },
+  ];
+  const showemployee = true;
   return (
     <div className="App">
-      {employee ? (
-        <>
-        <Employee />
-        <Employee />
-        <Employee />
-        <Employee />
-        <Employee />
-        </>
-      ):<h3>You can not see the Employees</h3>}
+      {showemployee ? (
+        nameList.map((student, index) => (
+          <Employee name={student.name} role={student.role} key={index} />
+        ))
+      ) : (
+        <p>You can not see Employee</p>
+      )}
     </div>
   );
 }
--- src/Employee.jsx
+++ src/Employee.jsx
@@ -1,4 +1,9 @@
-const Employee =()=>{
-    return <h1>Here is An Employee</h1>;
+const Employee = (props) => {
+  return (
+    <>
+      <h1>Here is An Employee {props.name}</h1>
+      <p>{props.role ? props.role : "No role"}</p>
+    </>
+  );
 };
 export default Employee;
