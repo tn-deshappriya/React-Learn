3eab6865dd68d7aefd7627e47e2191caa1976dbe
Mr.No-Hat <thiniranilushandeshappriya@gmail.com>
2025-04-01 04:55:54 +0530

    11.Create employee card using tailwind css.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [role, setRole] = useState();
   const showemployee = true;
   return (
-    <div className="App bg-red-300">
+    <div className="App">
       {showemployee ? (
         <>
           <input
@@ -15,11 +15,33 @@ function App() {
               setRole(e.target.value);
             }}
           />
-          <Employee name="Thinira" role="jun/Dev" />
-          <Employee name="Himasha" role="senior/Dev" />
-          <Employee name="Shavindu" role="jun/Devops" />
-          <Employee name="Supun" role="cyberSequrity" />
-          <Employee name="Chamo" role={role} />
+          <div className="flex flex-wrap justify-center">
+            <Employee
+              name="Thinira"
+              role="jun/Dev"
+              image="https://images.pexels.com/photos/20570035/pexels-photo-20570035/free-photo-of-a-young-woman-in-a-green-jacket-sitting-and-holding-a-fujifilm-x100f-digital-camera.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+            />
+            <Employee
+              name="Himasha"
+              role="senior/Dev"
+              image="https://images.pexels.com/photos/20570035/pexels-photo-20570035/free-photo-of-a-young-woman-in-a-green-jacket-sitting-and-holding-a-fujifilm-x100f-digital-camera.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+            />
+            <Employee
+              name="Shavindu"
+              role="jun/Devops"
+              image="https://images.pexels.com/photos/20570035/pexels-photo-20570035/free-photo-of-a-young-woman-in-a-green-jacket-sitting-and-holding-a-fujifilm-x100f-digital-camera.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+            />
+            <Employee
+              name="Supun"
+              role="cyberSequrity"
+              image="https://images.pexels.com/photos/20570035/pexels-photo-20570035/free-photo-of-a-young-woman-in-a-green-jacket-sitting-and-holding-a-fujifilm-x100f-digital-camera.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+            />
+            <Employee
+              name="Chamo"
+              role={role}
+              image="https://images.pexels.com/photos/20570035/pexels-photo-20570035/free-photo-of-a-young-woman-in-a-green-jacket-sitting-and-holding-a-fujifilm-x100f-digital-camera.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+            />
+          </div>
         </>
       ) : (
         <p>You can not See Employees</p>
--- src/Employee.jsx
+++ src/Employee.jsx
@@ -1,9 +1,24 @@
 const Employee = (props) => {
   return (
-    <>
-      <h1>Here is An Employee {props.name}</h1>
-      <p>{props.role ? props.role : "No role"}</p>
-    </>
+    <div className="m-2 flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 bg-white shadow-lg rounded-2xl max-w-lg">
+      {/* Profile Image */}
+      <img
+        className="object=cover rounded-full h-[100px] w-[100px] block mx-auto block h-24 rounded-full sm:mx-0 sm:shrink-0"
+        src={props.image}
+        alt="Erin Lindford"
+      />
+      {/* Profile Details */}
+      <div className="space-y-2 text-center sm:text-left">
+        <div className="space-y-0.5">
+          <p className="text-lg font-semibold text-black">{props.name}</p>
+          <p className="font-medium text-gray-500">{props.role}</p>
+        </div>
+        {/* Message Button */}
+        <button className="border border-purple-200 text-purple-600 px-4 py-2 rounded-lg hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700">
+          Message
+        </button>
+      </div>
+    </div>
   );
 };
 export default Employee;
